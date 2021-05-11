2b281b0cf5efa79b8bc6c224b4310b243eaa0bcd
happycooldude <pieterhuisman2000@gmail.com>
2021-05-11 10:22:41 +0200

    Home pagina gemaakt + selectie boxen werkend

---
+++ script.js
@@ -0,0 +1,68 @@
+const smPrice = 3;
+const mPrice = 5;
+const lPrice = 7;
+const toppingprice = 2;
+
+const types = ["Napoletana", "Margherita", "Quattro-Formaggi", "Funghi", "Calzone"];
+const toppings = ["No extra toppings", "Bacon", "Black olives", "Capers", "Anchovies", "Capsicum"]
+
+document.getElementById("test").onclick = getSize;
+document.getElementById("test2").onclick = getType;
+document.getElementById("test3").onclick = getToppings;
+document.getElementById("test4").onclick = getAmmount;
+document.getElementById("test5").onclick = getExtra;
+
+function getSize(){
+   if (document.getElementById("small").selected) {
+       console.log(smPrice);
+   }else if(document.getElementById("medium").selected){
+       console.log(mPrice);
+   }else if(document.getElementById("large").selected){
+       console.log(lPrice);
+   }
+}
+
+function getType(){
+    if (document.getElementById("Napoletana").selected) {
+        console.log(types[0])
+    }else if(document.getElementById("Margherita").selected){
+        console.log(types[1]);
+    }else if(document.getElementById("Quattro-Formaggi").selected){
+        console.log(types[2]);
+    }else if(document.getElementById("Funghi").selected){
+        console.log(types[3]);
+    }else if(document.getElementById("Calzone").selected){
+        console.log(types[4]);
+    }
+    
+}
+
+function getToppings(){
+    if (document.getElementById("nothing").selected) {
+        console.log(toppings[0]);
+    }else if(document.getElementById("bacon").selected){
+        console.log(toppings[1]);
+    }else if(document.getElementById("black-olives").selected){
+        console.log(toppings[2]);
+    }else if(document.getElementById("capers").selected){
+        console.log(toppings[3]);
+    }else if(document.getElementById("anchovies").selected){
+        console.log(toppings[4]);
+    }else if(document.getElementById("capsicum").selected){
+        console.log(toppings[5]);
+    }
+}
+
+function getAmmount(){
+
+}
+
+function getExtra(){
+    if(document.getElementById("card").checked){
+        console.log("card is checked")
+    }else if(document.getElementById("sliced").checked){
+        console.log("sliced is checked")
+    }else if(document.getElementById("home").checked){
+        console.log("home is checked")
+    }
+}
